f4a74c26891fc60f1df2d31895555e69d47c2ae2
Sayeed <sayeed@diit.info>
2015-04-02 16:50:07 +0600

    tt commit

---
+++ src/RuhitClass.java
@@ -0,0 +1,13 @@
+
+public class RuhitClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Ruhits Git 02/04/2015");
+		System.out.println("Ruhitss Git 02/04/2015");
+
+	}
+
+}
